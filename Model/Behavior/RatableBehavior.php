c933547dbbea8a74b960cf9796b33d0444fc27a0
euromark <euromark@web.de>
2012-07-28 12:33:05 +0200

    missing app uses

--- Model/Behavior/RatableBehavior.php
+++ Model/Behavior/RatableBehavior.php
@@ -8,6 +8,7 @@
  * @copyright Copyright 2010, Cake Development Corporation (http://cakedc.com)
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+App::uses('ModelBehavior', 'Model');
 
 /**
  * CakePHP Ratings Plugin
--- Model/Rating.php
+++ Model/Rating.php
@@ -8,6 +8,7 @@
  * @copyright Copyright 2010, Cake Development Corporation (http://cakedc.com)
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+App::uses('RatingsAppModel', 'Ratings.Model');
 
 /**
  * CakePHP Ratings Plugin
